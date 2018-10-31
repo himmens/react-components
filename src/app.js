bbb01213072e20f27521a863004e240107a3b856
s.himanych <s.himanych@zmeke.com>
2018-11-01 01:49:04 +0300

    webpack setup

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './app.css';
+
+export default function App() {
+    return (
+        <div className={styles.root}>
+            App
+        </div>
+    );
+}
